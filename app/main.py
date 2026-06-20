854088bd376c3269670c64d217b706100a0596bf
kohliwood18 <woodkohli@gmail.com>
2026-06-21 01:00:51 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,27 +15,30 @@ def main():
         with conn:
             while True:
                 request = conn.recv(1024)
-
                 if not request:
                     break
 
-                api_version = struct.unpack(">h", request[6:8])[0]
-                correlation_id = struct.unpack(">i", request[8:12])[0]
+                api_version = struct.unpack(">h",request[6:8])[0]
+
+                correlation_id = struct.unpack(">i",request[8:12])[0]
 
                 if 0 <= api_version <= 4:
                     error_code = 0
                 else:
                     error_code = 35
-
+                
                 response_body = (
-                    struct.pack(">h", error_code)
-                    + b"\x02"
-                    + struct.pack(">h", 18)
-                    + struct.pack(">h", 0)
-                    + struct.pack(">h", 4)
-                    + b"\x00"
-                    + struct.pack(">i", 0)
-                    + b"\x00"
+                    struct.pack(">h", error_code)      # error_code
+
+                    + b"\x02"                # compact array length = 1+1
+
+                    + struct.pack(">h", 18)   # api_key
+                    + struct.pack(">h", 0)    # min_version
+                    + struct.pack(">h", 4)    # max_version
+                    + b"\x00"                # tag buffer
+
+                    + struct.pack(">i", 0)    # throttle_time_ms
+                    + b"\x00"                # tag buffer
                 )
 
                 message_size = 4 + len(response_body)
