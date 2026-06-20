5a1687f2e1dcbfda31295c5edf243c1dec0fdde5
kohliwood18 <woodkohli@gmail.com>
2026-06-21 00:54:10 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,53 +9,53 @@ def main():
         ("localhost", 9092),
         reuse_port=True
     )
+    while True:
+        conn, addr = server.accept()
 
-    conn, addr = server.accept()
+        with conn:
+            while True:
+                request = conn.recv(1024)
+                if not request:
+                    break
 
-    with conn:
-        while True:
-            request = conn.recv(1024)
-            if not request:
-                continue
+            api_version = struct.unpack(
+                ">h",
+                request[6:8]
+            )[0]
 
-        api_version = struct.unpack(
-            ">h",
-            request[6:8]
-        )[0]
+            correlation_id = struct.unpack(
+                ">i",
+                request[8:12]
+            )[0]
 
-        correlation_id = struct.unpack(
-            ">i",
-            request[8:12]
-        )[0]
+            if 0 <= api_version <= 4:
+                error_code = 0
+            else:
+                error_code = 35
+            
+            response_body = (
+                struct.pack(">h", error_code)      # error_code
 
-        if 0 <= api_version <= 4:
-            error_code = 0
-        else:
-            error_code = 35
-        
-        response_body = (
-            struct.pack(">h", error_code)      # error_code
+                + b"\x02"                # compact array length = 1+1
 
-            + b"\x02"                # compact array length = 1+1
+                + struct.pack(">h", 18)   # api_key
+                + struct.pack(">h", 0)    # min_version
+                + struct.pack(">h", 4)    # max_version
+                + b"\x00"                # tag buffer
 
-            + struct.pack(">h", 18)   # api_key
-            + struct.pack(">h", 0)    # min_version
-            + struct.pack(">h", 4)    # max_version
-            + b"\x00"                # tag buffer
+                + struct.pack(">i", 0)    # throttle_time_ms
+                + b"\x00"                # tag buffer
+            )
 
-            + struct.pack(">i", 0)    # throttle_time_ms
-            + b"\x00"                # tag buffer
-        )
+            message_size = 4 + len(response_body)
 
-        message_size = 4 + len(response_body)
+            response = (
+                struct.pack(">i", message_size)
+                + struct.pack(">i", correlation_id)
+                + response_body
+            )
 
-        response = (
-            struct.pack(">i", message_size)
-            + struct.pack(">i", correlation_id)
-            + response_body
-        )
-
-        conn.sendall(response)
+            conn.sendall(response)
 
 
 if __name__ == "__main__":
