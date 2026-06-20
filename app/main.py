7c90124959779463818faa3116cd1a21a6a79ed7
kohliwood18 <woodkohli@gmail.com>
2026-06-20 15:35:55 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,14 +15,25 @@ def main():
     with conn:
         request = conn.recv(1024)
 
+        api_version = struct.unpack(
+            ">h",
+            request[6:8]
+        )[0]
+
         correlation_id = struct.unpack(
             ">i",
             request[8:12]
         )[0]
 
+        if 0 <= api_version <= 4:
+            error_code = 0
+        else:
+            error_code = 35
+
         response = (
-            struct.pack(">i", 0) +
-            struct.pack(">i", correlation_id)
+            struct.pack(">i", 0)
+            + struct.pack(">i", correlation_id)
+            + struct.pack(">h", error_code)
         )
 
         conn.sendall(response)
